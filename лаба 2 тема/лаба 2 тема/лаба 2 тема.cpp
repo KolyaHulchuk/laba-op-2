9f90844d45ca501b74c5ff41ad27a9859577db78
KolyaHulchuk <148907387+KolyaHulchuk@users.noreply.github.com>
2023-10-27 14:50:51 +0300

    laba op2
    laba op2

---
+++ лаба 2 тема/лаба 2 тема/лаба 2 тема.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+int main() {
+    int a, b, c, d;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введіть значення a, b, c та d: ";
+    std::cin >> a >> b >> c >> d;
+
+    int A_c = a / c;
+    int B_c = b / c;
+    int C_c = c / c;
+    int result = 0;
+
+    if ((A_c & B_c) || (A_c & C_c)) {
+        if (c < 0 && b != 0) {
+            result = a * std::pow(c, 2) + b * std::pow(c, 2);
+        }
+        else if (c > 0 && b == 0) {
+            result = (c - a) * (c + c - c) * 6;
+        }
+    }
+
+    if (result == 0) {
+        std::cout << "Дійсне значення" << std::endl;
+    }
+    else {
+        std::cout << "Результат: " << result << std::endl;
+    }
+
+    return 0;
+}
